f41425b13c23be2779943e8af6698e70cb4bddd7
Netfrux Mac <netfruxmac@Netfruxs-Mac-mini.local>
2025-03-10 12:44:22 +0530

    some other changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // ✅ Use "react-dom/client" in React 18
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
