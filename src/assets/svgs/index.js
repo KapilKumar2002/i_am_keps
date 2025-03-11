bd16e09ed1507de4d3df656f34f15a2f6468533f
Netfrux Mac <netfruxmac@Netfruxs-Mac-mini.local>
2025-03-11 13:17:11 +0530

    commit

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,15 +1,21 @@
 import nodejs from "./Node.js_logo.svg";
 import bootstrap from "./bootstrap.svg";
+import tailwind from "./tailwind.svg";
 import docker from "./Docker.svg";
 import html from "./html-5.svg";
+import css from "./css.svg";
 import mongodb from "./MongoDB.svg";
 import mysql from "./mysql.svg";
 import postgresql from "./postgresql-icon.svg";
 import python from "./Python.svg";
+import cpp from "./c++.svg";
 import sqlite from "./sqlite.svg";
 import flutter from "./flutter.svg";
 import firebase from "./firebase.svg";
 import django from "./django.svg";
+import dart from "./dart.svg";
+import js from "./js.svg";
+import react from "./react.svg";
 
 export {
   nodejs,
@@ -24,4 +30,10 @@ export {
   flutter,
   firebase,
   django,
+  dart,
+  js,
+  react,
+  cpp,
+  tailwind,
+  css,
 };
