79274fa2ec6480e8314979777b66e40e43fd76f2
KapilKumar2002 <kapil012002@gmail.com>
2025-03-15 22:05:51 +0530

    commit

--- src/components/atoms/Loader/index.jsx
+++ src/components/atoms/Loader/index.jsx
@@ -3,14 +3,14 @@ import { ThreeDots } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-screen flex-col text-orange-500 px-4">
+    <div className="flex justify-center items-center h-screen flex-col text-orange-500 px-8">
       <ThreeDots
         height="80"
         width="80"
         color="orange"
         ariaLabel="loading-indicator"
       />
-      <div className="text-lg">
+      <div className="text-lg text-center">
         Please wait, while I am loading Kapil Kumar...
       </div>
     </div>
